d95d790c2ec953d8c079010cfe0c46a3010a15ef
Stellberry <76648418+Stellberry@users.noreply.github.com>
2026-02-14 20:47:26 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,75 @@
+function selectOption(option) {
+    
+    if (option === 'yes') {
+        
+        flashRainbowColors(function() {
+            document.getElementById('question').style.display = 'none';
+            displayMilkHug();
+        });
+    } else if (option === 'no') {
+        
+        document.getElementById('no-button').innerText = 'You sure?'; 
+        
+        var yesButton = document.getElementById('yes-button');
+        var currentFontSize = window.getComputedStyle(yesButton).getPropertyValue('font-size');
+        var newSize = parseFloat(currentFontSize) * 2; 
+        yesButton.style.fontSize = newSize + 'px';
+    } else {
+        
+        alert('Invalid option!');
+    }
+}
+
+
+function flashRainbowColors(callback) {
+    var colors = ['#ff0000', '#ff7f00', '#ffff00', '#00ff00', '#0000ff', '#4b0082', '#9400d3'];
+    var i = 0;
+    var interval = setInterval(function() {
+        document.body.style.backgroundColor = colors[i];
+        i = (i + 1) % colors.length;
+    }, 200);
+    setTimeout(function() {
+        clearInterval(interval);
+        document.body.style.backgroundColor = '';
+        if (callback) {
+            callback();
+        }
+    }, 2000);
+}
+
+function displayMilkSquish() {
+    
+    var imageContainer = document.getElementById('image-container');
+    
+    var milkImage = new Image();
+    
+    milkImage.src = 'MilkHug.gif';
+    
+    milkImage.alt = 'Cat';
+    
+    milkImage.onload = function() {
+        imageContainer.appendChild(milkImage);
+    };
+}
+
+function displayMilkHug() {
+   
+    document.getElementById('image-container').innerHTML = '';
+    
+    var imageContainer = document.getElementById('image-container');
+    
+    var hugImage = new Image();
+    
+    hugImage.src = 'MilkHug.gif'; 
+   
+    hugImage.alt = 'Milk Hug Mocha';
+    
+    hugImage.onload = function() {
+        imageContainer.appendChild(hugImage);
+        
+        document.getElementById('options').style.display = 'none';
+    };
+}
+
+// Display the cat.gif initially
+displayMilkSquish();
